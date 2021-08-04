a256917e0bd6ec88696266eda19892b35e466ebb
towel1017 <48384442+towel1017@users.noreply.github.com>
2021-08-04 23:50:50 +0900

    test: 시작페이지 렌더링 테스트 코드 작성 - #1

--- test/Components/StartPage/StartPage.test.tsx
+++ test/Components/StartPage/StartPage.test.tsx
@@ -2,4 +2,12 @@ import React from "react";
 
 import { render } from "../../test-utils";
 
-describe("StartPage tests", () => {});
+import HomeKeeper from "../../../pages";
+
+describe("StartPage tests", () => {
+  it("StartPage render tests", () => {
+    const { container } = render(<HomeKeeper />);
+
+    expect(container).toHaveTextContent("우리집의 작은 경제 지킴이");
+  });
+});
