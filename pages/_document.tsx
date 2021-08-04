df0591104158b145aa7341abdd0e3a1961a81fbb
towel1017 <48384442+towel1017@users.noreply.github.com>
2021-08-04 15:08:38 +0900

    init

---
+++ pages/_document.tsx
@@ -0,0 +1,41 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () => originalRenderPage({});
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <title>홈키퍼</title>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Rank: React.FC = () => {
+  return (
+    <>
+      <span>홈키퍼</span>
+    </>
+  );
+};
+
+export default Rank;
