7918a9692c5ec5cc9b05efb08cf2bc357990a421
towel1017 <48384442+towel1017@users.noreply.github.com>
2021-08-05 17:22:38 +0900

    test(test): Header 우측 Today Date 테스트 코드 작성 - #2

--- test/Components/MainPage/Header/Header.test.tsx
+++ test/Components/MainPage/Header/Header.test.tsx
@@ -2,4 +2,13 @@ import React from "react";
 
 import { render } from "../../../test-utils";
 
-describe("Header tests", () => {});
+import Main from "@pages/main";
+
+describe("Header tests", () => {
+  it("Header renders today date", () => {
+    const { container } = render(<Main />);
+    const today = new Date();
+    const dateArr = [today.getFullYear(), today.getMonth() + 1, today.getDate()];
+    expect(container).toHaveTextContent(`Today : ${dateArr.join(".")}`);
+  });
+});
